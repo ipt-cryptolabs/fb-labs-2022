42a29aa98a850c892ffb7d6f0df1257a72c9253b
whois_PC <dmytrenkodaniel@gmail.com>
2022-09-27 20:13:17 +0300

    Read example txt and write clear text to files

--- cp_1/lab_1.py
+++ cp_1/lab_1.py
@@ -1,1 +1,19 @@
+import re
+
 alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+alphabetS = 'абвгдежзийклмнопрстуфхцчшщъыьэюя '
+
+# відкрили файл, та  відформатували текст, отримали текст з пробілами, та текст - без
+with open('cp_1/ex.txt', 'r', encoding='utf-8') as file:
+    plainText = file.read().lower().replace('ъ', 'ь').replace('ё', 'е')
+    plainText = re.compile(r'\s*[^а-яА-Я ]').sub('', plainText)
+    reg = re.compile('[^а-яА-Я]')
+    noSpacesText = reg.sub('', plainText)
+
+with open('cp_1/plainText.txt', 'w', encoding='utf-8') as file:
+    file.writelines(plainText)    
+with open('cp_1/noSpacesText.txt', 'w', encoding='utf-8') as file:
+    file.writelines(noSpacesText)
+
+print(plainText + '\n')
+print(noSpacesText)
