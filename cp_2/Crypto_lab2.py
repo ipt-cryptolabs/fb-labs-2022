9a30e0477ff99d4e321cb8d901fecc6b682d929e
Sanya <alex0303275@gmail.com>
2022-10-04 21:12:09 +0300

    add text files + make ident_key func (doesn't work)

--- cp_2/Crypto_lab2.py
+++ cp_2/Crypto_lab2.py
@@ -2,6 +2,29 @@ import re
 from statistics import mean
 
 
+def ident_key(text, key_length):
+    alletters = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+                 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+    key = []
+    for i in range(key_length):
+        key.append(alletters[abs(alletters.index('о') - alletters.index(
+            list(countletterfrequency(text[i::key_length]).keys())[0]))])
+    print(key)
+    return key
+
+
+def countletterfrequency(text):
+    alletters = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
+                 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
+    letterfrequency = dict.fromkeys(alletters, 0)
+    for i in text:
+        if i in alletters:
+            letterfrequency[i] += 1
+    max_val = max(letterfrequency.values())
+    final_dict = {k: v for k, v in letterfrequency.items() if v == max_val}
+    return final_dict
+
+
 def hit_index_count(text):
     alletters = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й',
                  'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
@@ -18,6 +41,7 @@ def hit_index_count(text):
     for i in frequency:
         hit_index += (i * (i + 1)) / (len(text) * (len(text) + 1))
     # print(hit_index)
+    # print(frequency)
     return hit_index
 
 
@@ -57,17 +81,15 @@ def guess_key_period(text, key_length):
     block_indexes = []
     for i in range(key_length):
         block_indexes.append(hit_index_count(text[i::key_length]))
-    # print(block_indexes)
-    # print(mean(block_indexes))
     return mean(block_indexes)
 
 
 def calc_key_length(text):
     # ref_text = open("refactoredtext.txt", 'r').read()
-    I_THEORY = 0.0553
+    I_THEORY = 0.066
     I_for_key_length = []
     number = 0
-    for i in range(2, 30):
+    for i in range(2, 15):
         I_for_key_length.append(guess_key_period(text, i))
     return I_for_key_length.index(min(I_for_key_length, key=lambda n: (abs(I_THEORY - n), n))) + 2
     # print(I_for_key_length)
@@ -75,12 +97,13 @@ def calc_key_length(text):
     # print(I_for_key_length.index(number))
     # return I_for_key_length.index(number)
 
-# uncleantext = open("sourcetext_long.txt", encoding='utf-8').read()
+
+# uncleantext = open("lab_text.txt", encoding='utf-8').read()
 # cleantext = re.sub('[^ А-Яа-я\nёЁ]+', '', uncleantext)
-# cleantext = cleantext.replace('\n', ' ')
+# cleantext = cleantext.replace('\n', '')
 # cleantext = re.sub(' +', ' ', cleantext)
 # cleantext = cleantext.lower()
-# open('refactoredtext_long.txt', 'w').write(cleantext)
+# open('lab_text_refactored.txt', 'w').write(cleantext)
 
 two_let_key = 'бу'
 three_let_key = 'хог'
@@ -88,20 +111,10 @@ four_let_key = 'леон'
 five_let_key = 'спайк'
 big_let_key = 'отченашсущийнаенаекядвщйьмрвв'
 ref_text = open("refactoredtext.txt", 'r').read()
-encryption(create_key(three_let_key, open("refactoredtext.txt", 'r').read()), ref_text)
+
+encryption(create_key(five_let_key, ref_text), ref_text)
 cipher_text = open("ciphered_text.txt", 'r').read()
-# guess_key_period(cipher_text, 5)
-# print(hit_index_count(ref_text))
+print(ident_key(cipher_text, calc_key_length(cipher_text)))
+# lab_text = open("lab_text_refactored.txt", 'r').read()
+# print(calc_key_length(cipher_text))
 
-# for i in range(0,len(text),2):
-# block_text.append(i)
-
-# print(text[2::3])
-# print(text[3::3])
-
-# print(block_text)
-# decryption(create_key(big_let_key, open("ciphered_text.txt", 'r').read()), open("ciphered_text.txt", 'r').read())
-# hit_index_count(open("ciphered_text.txt", 'r').read())
-# hit_index_count(open("refactoredtext.txt", 'r').read())
-lab_text = open("lab_text.txt", 'r').read()
-print(calc_key_length(cipher_text))
