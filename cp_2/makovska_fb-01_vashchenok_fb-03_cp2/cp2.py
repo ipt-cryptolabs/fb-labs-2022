9b181552952f969c8f8e66cc2cee6d026518cafd
dvashchenok <99654452+dvashchenok@users.noreply.github.com>
2022-10-09 12:32:40 +0300

    Update cp2.py

--- cp_2/makovska_fb-01_vashchenok_fb-03_cp2/cp2.py
+++ cp_2/makovska_fb-01_vashchenok_fb-03_cp2/cp2.py
@@ -1,4 +1,28 @@
+import re
+import math
 
+alfavit = 'абвгдеёэжзиыйклмнопрстуфхцчшщъьюя'
+alfavit_probil = 'абвгдеёэжзиыйклмнопрстуфхцчшщъьюя '
+
+
+def filter_txt(file_name):
+    # зчитуємо текст
+    file = open(file_name, encoding="utf8")
+    t = file.read()
+    file.close()
+    # переводимо всі літери в нижній регістр і оброблюємо
+    lowered_text = t.lower()
+    new_txt = re.sub(r'[^а-яё ]', '', lowered_text)  # прибрати всі символи, які не схожі на літери нашого алфавіту(наприкл позначення переходу на новий рядок \n)
+    # записуємо оброблений текст у файл
+    new_file = open('filtered_d.txt', 'w')
+    new_file.write(new_txt)
+    new_file.close()
+    
+
+filter_txt('ourtext.txt')
+file_d = open("filtered_d.txt")
+filtered_d = file_d.read()
+file_d.close()
 
 
 
