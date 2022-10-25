e413b58098fd9159f1caafa5966b36b3a57bac5c
Daniil D <dmytrenkodaniel@gmail.com>
2022-10-26 00:25:01 +0300

    Added key_gen

--- cp_2/dmytrenko_fb-04_serbinenko_fb-04/main.py
+++ cp_2/dmytrenko_fb-04_serbinenko_fb-04/main.py
@@ -1,12 +1,37 @@
-file = open('se.txt').read().lower().replace('\n', '').replace(' ', '').replace('ё', 'е').replace('ъ', 'ь')
-alphabet = 'абвгдежзийклмнопрстуфхцчшщыьэюя '
+import random
 
-txt = ''
-for i in file:
-    if i in alphabet:
-        txt += i
+# file = open('se.txt').read().lower().replace('\n', '').replace(' ', '').replace('ё', 'е').replace('ъ', 'ь')
 
-print(txt)
+alphabet = 'абвгдежзийклмнопрстуфхцчшщыьэюя'
 
-with open('pt.txt', 'w') as file:
-    file.write(txt)
+# txt = ''
+# for i in file:
+#     if i in alphabet:
+#         txt += i
+
+# print(txt)
+
+# with open('pt.txt', 'w') as file:
+#     file.write(txt)
+
+keys = []
+
+def key_gen():
+    for i in range(2, 6):
+        j = 0
+        key= ''
+        while j < i:
+            key += ''.join(random.choice(alphabet))
+            j += 1
+        keys.append(key)
+
+    for i in range(10, 21):
+        key= ''
+        j = 0
+        while j < i:
+            key += ''.join(random.choice(alphabet))
+            j += 1
+        keys.append(key)
+
+key_gen()
+print(keys)
