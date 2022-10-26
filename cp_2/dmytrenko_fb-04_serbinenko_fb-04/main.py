7db8cbeeda84def80e74f89e5a490bb9bf72a999
whois_PC <dmytrenkodaniel@gmail.com>
2022-10-26 18:41:45 +0300

    added search+index, modified encrypt

--- cp_2/dmytrenko_fb-04_serbinenko_fb-04/main.py
+++ cp_2/dmytrenko_fb-04_serbinenko_fb-04/main.py
@@ -16,6 +16,7 @@ alphabet = 'абвгдежзийклмнопрстуфхцчшщыьэюя'
 #     file.write(txt)
 
 text = open('pt.txt', 'r', encoding='utf-8').read()
+var_text = open('var_text.txt', 'r', encoding='utf-8').read()
 keys = []
 
 def key_gen():
@@ -37,12 +38,7 @@ def key_gen():
 
 def encrypt(txt: str, keys: list):
     with open('encrypted_text.txt', 'w+', encoding='utf-8') as file:
-        plait_text = Counter(txt)
-        index = 0
-        for i in plait_text:
-            index += plait_text[i]*(plait_text[i] - 1)
-        text_sum = sum(plait_text.values())
-        index /= text_sum*(text_sum - 1)
+        index = index_counter(txt)
         file.write(f'Our plain text\nindex: {index}\ntext:\n{txt}\n\n')
         
         for key in keys:
@@ -51,13 +47,35 @@ def encrypt(txt: str, keys: list):
                 letter = alphabet[(alphabet.index(txt[i]) + alphabet.index(key[i % len(key)]))%len(alphabet)]
                 encrypted.append(letter)
             enc_txt = ''.join(encrypted)
-            enc_dict = Counter(encrypted)
-            index = 0
-            for i in enc_dict:
-                index += enc_dict[i]*(enc_dict[i] - 1)
-            dict_sum = sum(enc_dict.values())
-            index /= dict_sum*(dict_sum - 1)
+            index = index_counter(encrypted)
             file.write(f'key: {key}\nindex: {index}\ntext:\n{enc_txt}\n\n')
 
-key_gen()
-encrypt(text, keys)
+def index_counter(txt: str):
+    plait_text = Counter(txt)
+    index = 0
+    for i in plait_text:
+        index += plait_text[i]*(plait_text[i] - 1)
+    text_sum = sum(plait_text.values())
+    index /= text_sum*(text_sum - 1)
+    return index
+
+# key_gen()
+# encrypt(text, keys)
+
+def search():
+    count = {}
+    for i in range(1, 31):
+        blocks = []
+        for j in range(i):
+            blocks.append(var_text[j::i])
+        index = 0
+        for block in blocks:
+            index += index_counter(block)
+        index /= i
+        count[i] = index
+    return count
+
+index_dict = search()
+# print(max(index_dict.values()))
+
+key_len = max(index_dict, key=index_dict.get)
