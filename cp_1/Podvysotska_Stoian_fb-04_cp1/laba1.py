8ee66019aba3f0d06878ffd08d3ae306aed41c62
podol-ipt <podol-ipt24@lll.kpi.ua>
2022-10-26 22:41:38 +0300

    bigrams fixed

--- cp_1/Podvysotska_Stoian_fb-04_cp1/laba1.py
+++ cp_1/Podvysotska_Stoian_fb-04_cp1/laba1.py
@@ -51,7 +51,7 @@ def coolBigrams(file, intersections):
     else:
         bigrams = [text[i:i+2] for i in range (0, len(text), 2)]
     counts = Counter(bigrams)
-    frequency = {item: counts.get(item) / len(text) for item in counts.keys()}
+    frequency = {item: counts.get(item) / len(bigrams) for item in counts.keys()}
     frequency = dict(reversed(sorted(frequency.items(), key=lambda item: item[1])))
     entropy = 0.5 * sum(list(-freq * math.log2(freq) for freq in frequency.values()))
     if len(counts) == 32:
@@ -73,7 +73,7 @@ coolMonograms('without_spaces.txt')
 print('BIGRAMS WITH SPACES', '\n')
 coolBigrams('with_spaces.txt', False)
 
-print('BIGRAMS WITHOUT SPACES', '\n\n')
+print('BIGRAMS WITHOUT SPACES', '\n')
 coolBigrams('without_spaces.txt', False)
 
 print('BIGRAMS WITH SPACES & WITH INTERSECTIONS', '\n')
