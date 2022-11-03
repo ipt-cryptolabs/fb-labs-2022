a51377f65262dceeb091c78fd23e83c455769d51
Kaseki1 <kaseki1@protonmail.com>
2022-11-03 16:40:19 +0200

    First version of implementation of period counting function

--- cp_2/huzenkov_fb-03/count_period.py
+++
@@ -1,3 +0,0 @@
-def count_period(cypher_text):
-    r = 0
-    return r
---
+++ cp_2/huzenkov_fb-03/vigenere.py
@@ -0,0 +1,43 @@
+alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
+max_key_len = 128
+
+
+def encode(text, key):
+    key_length = len(key)
+    cypher_text = ''
+    for i in range(len(text)):
+        cypher_text += chr((alphabet.index(text[i]) + alphabet.index(key[i % key_length])) % len(alphabet) + 1072)
+    return cypher_text
+
+
+def count_frequencies(text):
+    freq = dict()
+    for i in text:
+        if i not in freq:
+            freq[i] = 1
+        else:
+            freq[i] += 1
+    return freq
+
+
+def count_index(text):
+    text_len = len(text)
+    sum_of_frequencies = 0
+    freqs = count_frequencies(text)
+    for i in freqs:
+        sum_of_frequencies += freqs[i] * (freqs[i] - 1)
+    index = sum_of_frequencies/(text_len * (text_len - 1))
+    return index
+
+
+def count_period(cypher_text, theoretical_index):
+    indexes = []
+    differences = []
+    for r in range(2, max_key_len):
+        block = ''
+        for j in range(0, len(cypher_text), r):
+            block += cypher_text[j]
+        indexes.append(count_index(block))
+    for i in indexes:
+        differences.append(abs(theoretical_index - i))
+    return differences.index(min(differences)) + 2
