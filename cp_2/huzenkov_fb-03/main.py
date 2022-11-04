e01eb0baf2654603db90a5d969d778900607b7cb
Kaseki1 <kaseki1@protonmail.com>
2022-11-04 18:51:33 +0200

    Implemented all functions except Caesar key break

--- cp_2/huzenkov_fb-03/main.py
+++ cp_2/huzenkov_fb-03/main.py
@@ -1,5 +1,4 @@
 import re
-
 import vigenere
 from vigenere import *
 
@@ -10,10 +9,21 @@ text = text.lower()
 text = re.sub("[^А-аЯ-я]", "", text)
 language_reference = count_frequencies(text)
 index = count_index(text)
-key = 'живилюбикпи'
-print('Довжина ключа:', len(key))
-cypher_text = encrypt(text, key)
-r = count_period(cypher_text, index)
-print('Обчислена довжина ключа:', r)
-key = find_key(cypher_text, r, language_reference)
-print('Знайдений ключ:', key)
+keys = ['зе', 'всу', 'воин', 'казак', 'живилюбикпи']
+cyphers = []
+cyphers_indexes = []
+for key in keys:
+    cyphers.append(encrypt(text, key))
+for cypher in cyphers:
+    cyphers_indexes.append(count_index(cypher))
+print('Індекс відповідності відкритого тексту:', index)
+for i in range(len(cyphers_indexes)):
+    print('Індекс відповідності для шифротексту з ключем довжиною ', len(keys[i]), ': ', cyphers_indexes[i], sep='')
+
+print()
+
+variant_encrypted = open('variant.txt', 'r').read()
+variant_key = find_key(variant_encrypted, count_period(variant_encrypted, index), language_reference)
+variant_decrypted = decrypt(variant_encrypted, variant_key)
+open('variant_decrypted.txt', 'w').write(variant_decrypted)
+print('Значение ключа для заданного варианта:', variant_key)
--- cp_2/huzenkov_fb-03/vigenere.py
+++ cp_2/huzenkov_fb-03/vigenere.py
@@ -1,4 +1,3 @@
-from copy import deepcopy
 alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюя'
 max_key_len = 128
 
@@ -22,7 +21,7 @@ def decrypt(text, key):
 def count_frequencies(text):
     freq = dict()
     for i in text:
-        if i not in freq:
+        if i not in freq.keys():
             freq[i] = 1
         else:
             freq[i] += 1
@@ -32,9 +31,9 @@ def count_frequencies(text):
 def count_index(text):
     text_len = len(text)
     sum_of_frequencies = 0
-    freqs = count_frequencies(text)
-    for i in freqs:
-        sum_of_frequencies += freqs[i] * (freqs[i] - 1)
+    frequencies = count_frequencies(text)
+    for i in frequencies:
+        sum_of_frequencies += frequencies[i] * (frequencies[i] - 1)
     index = sum_of_frequencies/(text_len * (text_len - 1))
     return index
 
@@ -60,32 +59,20 @@ def count_period(cypher_text, theoretical_index):
     return differences.index(min(differences)) + 2
 
 
-def find_caesar_key(cypher_text, language_reference):
+def most_common(frequency_table):
+    most_frequent = ''
+    frequency = -1
+    for key, value in frequency_table.items():
+        if value > frequency:
+            frequency = value
+            most_frequent = key
+    return most_frequent
+
+
+def find_caesar_key(cypher_text, reference_most_frequent):
     frequencies = count_frequencies(cypher_text)
-    keys = ''
-    for i in range(len(alphabet)-1):
-        reference_most_frequent = ''
-        cypher_text_most_frequent = ''
-        max = 0
-        for key, value in language_reference.items():
-            if value > max:
-                reference_most_frequent = key
-                max = value
-        max = 0
-        for key, value in frequencies.items():
-            if value > max:
-                cypher_text_most_frequent = key
-                max = value
-        keys += chr(abs(alphabet.index(reference_most_frequent) - alphabet.index(cypher_text_most_frequent)) % len(alphabet) + 1072)
-        frequencies.pop(cypher_text_most_frequent)
-        language_reference.pop(reference_most_frequent)
-    keys = count_frequencies(keys)
-    key = ''
-    max = 0
-    for k, v in keys.items():
-        if v > max:
-            key = k
-            max = v
+    cypher_most_frequent = most_common(frequencies)
+    key = chr((alphabet.index(cypher_most_frequent) - alphabet.index(reference_most_frequent)) % len(alphabet) + 1072)
     return key
 
 
@@ -93,11 +80,12 @@ def find_key(cypher_text, key_length, language_reference):
     ct_len = len(cypher_text)
     key = ''
     blocks = []
-    for i in range(key_length):
+    for i in range(0, key_length):
         block = ''
         for j in range(i, ct_len, key_length):
             block += cypher_text[j]
         blocks.append(block)
+    most_frequent = most_common(language_reference)
     for b in blocks:
-        key += find_caesar_key(b, deepcopy(language_reference))
+        key += find_caesar_key(b, most_frequent)
     return key
